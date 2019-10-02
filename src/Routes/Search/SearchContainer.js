0c9a8748e08ce4d77290fa067293c608ad2c317b
samuelmo <samuelmo78@gmail.com>
2019-10-02 02:26:38 -0600

    finished containers

---
+++ src/Routes/Search/SearchContainer.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import SearchPresenter from "./SearchPresenter"
+import { moviesApi, tvApi } from '../../api'
+
+export default class extends Component {
+    state = {
+        movieResults: null,
+        tvResults: null,
+        searchTerm: "",
+        loading: false,
+        error: null
+    }
+
+    handleSubmit = () => {
+        const { searchTerm } = this.state
+        if (searchTerm !== "") {
+            this.searchByTerm()
+        }
+    }
+
+    searchByTerm = async () => {
+        const { searchTerm } = this.state
+        this.setState({ loading: true })
+        try {
+            const {
+              data: { results: movieResults }
+            } = await moviesApi.search(searchTerm);
+            const {
+              data: { results: tvResults }
+            } = await tvApi.search(searchTerm);
+            //console.log({ movieResults, tvResults })
+            this.setState({
+              movieResults,
+              tvResults
+            });
+      
+
+        } catch {
+            this.setState({ error: "Can't find results." })
+        } finally {
+            this.setState({ loading: false })
+        }
+    }
+    
+    render() {
+        const { movieResults, tvResults, searchTerm, loading, error } = this.state
+        return(
+            <SearchPresenter 
+                movieResults={movieResults}
+                tvResults={tvResults}
+                searchTerm={searchTerm}
+                loading={loading}
+                error={error}
+                handleSubmit={this.handleSubmit}
+            />
+        )
+    }
+}
