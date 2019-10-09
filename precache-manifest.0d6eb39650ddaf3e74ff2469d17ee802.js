87acd12a632e46682966e0241d9ff43673ad7af7
samuelmo <samuelmo78@gmail.com>
2019-10-09 12:39:36 -0600

    Updates

---
+++ precache-manifest.0d6eb39650ddaf3e74ff2469d17ee802.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9305c607fa28b92103abe30a391cc4db",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "49f4360a860c47989aad",
+    "url": "/movie_app_2019/static/js/2.27abb9ae.chunk.js"
+  },
+  {
+    "revision": "ed45a853d07466abf891",
+    "url": "/movie_app_2019/static/js/main.a738f43e.chunk.js"
+  },
+  {
+    "revision": "2c8047aec7113be83a07",
+    "url": "/movie_app_2019/static/js/runtime-main.160ac167.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/movie_app_2019/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
